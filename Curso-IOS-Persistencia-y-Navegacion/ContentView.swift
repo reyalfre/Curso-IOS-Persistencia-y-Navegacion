7b40d83b4be99b08f5264950b2409ba2687ed9c1
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-02-04 12:25:37 +0100

    Initial Commit

---
+++ Curso-IOS-Persistencia-y-Navegacion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curso-IOS-Persistencia-y-Navegacion
+//
+//  Created by Equipo 8 on 4/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curso-IOS-Persistencia-y-Navegacion/Curso_IOS_Persistencia_y_NavegacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_IOS_Persistencia_y_NavegacionApp.swift
+//  Curso-IOS-Persistencia-y-Navegacion
+//
+//  Created by Equipo 8 on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_IOS_Persistencia_y_NavegacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
